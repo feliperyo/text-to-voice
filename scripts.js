319d404a0f48e830452d15121c10aa3d97674f9b
Felipe Ryo <ryofeliperyo@gmail.com>
2025-01-07 12:02:13 -0300

    adicionando vozes

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const text = document.querySelector("#text")
+const upload = document.querySelector("#upload")
+const voice = document.querySelector("#voice")
+const listenBtn = document.querySelector("#listen-btn")
+const downloadBtn = document.querySelector("#download-btn")
+
+const speak = new SpeechSynthesisUtterance();
+
+let availableVoices = [];
+
+const attValues = () => {
+    availableVoices = window.speechSynthesis.getVoices()
+
+    speak.voices = availableVoices[0]
+
+    console.log(availableVoices)
+
+    availableVoices.forEach((voices, index) => {
+        const option = document.createElement("option")
+        option.value = index
+        option.textContent = voices.name
+        voice.appendChild(option)
+    })
+}
+
+window.speechSynthesis.onvoiceschanged = attValues
